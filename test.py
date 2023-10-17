19c9f133a4cbaab83161df9fd09d8b18c609413a
Deal0308 <141692520+Deal0308@users.noreply.github.com>
2023-10-16 21:56:51 -0500

    first

---
+++ test.py
@@ -0,0 +1,37 @@
+print ("Hello World!")
+#JS let varname = value
+# decimal
+temperature = 98.8
+#string
+name = "John"
+#number
+number = 1
+
+#boolean
+bol = True
+print (bol)
+
+#addition
+result = 10 + 5
+#subtraction
+result = 10 - 5
+#multiplication
+result = 10 * 5
+#division
+result = 10 / 5
+
+x = 10
+
+# JS for (let i = 0; i < 10; i++)
+for i in range(10):
+    print (i)
+
+# arrays in Python are called lists
+fruits = ["apple", "banana", "cherry"]
+print (fruits[0])
+
+# function nameOf()
+#print is like the console log
+def greet(name):
+    print (f"Hello, {name}!")
+greet("John")
